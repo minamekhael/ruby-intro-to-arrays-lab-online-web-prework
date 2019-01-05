1fdb62b89de9eafc429c38af2768bd301bb2613d
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-05 22:02:44 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,45 @@
+def instantiate_new_array
+  # Created an empty array
+  array = []
+end
+
+def array_with_two_elements
+  # Defined array with two elements
+  array = ["wow", "shamwow"]
+end
+
+def first_element(num)
+  # Created array with three elementsand called on the first "Welcome to New York"
+  array = ["Welcome to New York", "wow", "frosted flakes"]
+  array[0]
+end
+
+def third_element(num)
+  # Created array with four elements and called on the third "Style"
+  array = ["shamwow", "wow", "Style", "Fashion"]
+  array[2]
+end
+
+def last_element(num)
+  # Created array with three elements and called on the last "Out of The Woods"
+  array = ["shamwow", "wow", "Out of The Woods"]
+  array[2]
+end
+
+def first_element_with_array_methods(first_country)
+  # Created array with four elements and called on the first "Thailand"
+  array = ["Thailand", "Egypt", "France", "Germany"]
+  array[0]
+end
+
+def last_element_with_array_methods(last_country)
+  # Created array listing four countries and called on the last "Myanmar"
+  array = ["Thailand", "Egypt", "France", "Myanmar"]
+  array[3]
+end
+
+def length_of_array(x)
+  # Created array with eight elements and displayed the number of elements using .length
+  array = ["Thailand", "Egypt", "France", "Myanmar", "Germany", "Spain", "Mexico", "Canada"]
+  array.length 
+end
