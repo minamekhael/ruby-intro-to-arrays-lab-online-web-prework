3947b89d925702dd8f5ed54311c1b897a4ff5598
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-05 21:47:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = ["wow", "shamwow"]
+end
+
+def first_element(num)
+  array = ["Welcome to New York", "wow", "frosted flakes"]
+  array[0]
+end
+
+def third_element(num)
+  array = ["shamwow", "wow", "Style", "Fashion"]
+  array[2]
+end
+
+def last_element(num)
+  array = ["shamwow", "wow", "Style"]
+  array[2]
+end
